904af40f34495201a243aeda18f30c88b94db7d3
keberelf058725 <kcaldonjr@gmail.com>
2022-08-20 16:14:02 -0400

    Additional framework including a home screen with a nav bar

--- handicaps/caps/models.py
+++ handicaps/caps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class player(models.Model):
+    name = models.CharField(max_length=100)
+    round_date = models.DateField()
+    score = models.IntegerField()
