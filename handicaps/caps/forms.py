8ed33c873a5a82c85b89a42d69885a345e0b0d95
keberelf058725 <kcaldonjr@gmail.com>
2022-08-21 20:32:43 -0400

    created outline for upload view and functions for processing database scores

--- handicaps/caps/forms.py
+++ handicaps/caps/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import FileExtensionValidator
 from django.contrib.auth.models import User
 
 
